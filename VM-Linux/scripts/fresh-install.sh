a90b0e2a1ec5a9cdb438985c5a510f6136a39f65
juronja <101410098+juronja@users.noreply.github.com>
2024-01-21 18:49:51 +0100

    added variables

--- VM-Linux/scripts/fresh-install.sh
+++ VM-Linux/scripts/fresh-install.sh
@@ -4,6 +4,11 @@
 # Author: juronja
 # License: MIT
 
+# Variables:
+user="$(whoami)"
+allowtcp="22,81"
+allowudp="51820"
+
 # Update and install upgrades
 sudo apt update -y && sudo apt upgrade
 
@@ -17,7 +22,7 @@ sudo timedatectl set-timezone Europe/Ljubljana
 sudo sed -i 's/\/\/Unattended-Upgrade::Automatic-Reboot-Time/Unattended-Upgrade::Automatic-Reboot-Time/' /etc/apt/apt.conf.d/50unattended-upgrades
 
 # Configure firewall
-#sudo ufw default allow outgoing && sudo ufw default deny incoming && sudo ufw allow 22,81/tcp && sudo ufw allow 51820/udp && sudo ufw enable
+#sudo ufw default allow outgoing && sudo ufw default deny incoming && sudo ufw allow $allowtcp/tcp && sudo ufw allow $allowudp/udp && sudo ufw enable
 
 # Disable pings in firewall
 #sudo sed -i 's/-A ufw-before-input -p icmp --icmp-type echo-request -j ACCEPT/-A ufw-before-input -p icmp --icmp-type echo-request -j DROP/' /etc/ufw/before.rules
@@ -27,8 +32,9 @@ sudo apt install qemu-guest-agent -y
 
 # Remove legacy Docker / Add Docker apt repository / Install Docker
 for pkg in docker.io docker-doc docker-compose dcker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-sudo apt-get update && sudo apt-get install ca-certificates curl gnupg && sudo install -m 0755 -d /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg && sudo chmod a+r /etc/apt/keyrings/docker.gpg && echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-sudo usermod -aG docker juronja
+sudo apt-get update && sudo apt-get install ca-certificates curl gnupg && sudo install -m 0755 -d /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg && sudo chmod a+r /etc/apt/keyrings/docker.gpg && echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -aG docker $user
 sudo docker run --name hello hello-world && sudo docker rm hello && sudo docker rmi hello-world
 
 # Reboot system
