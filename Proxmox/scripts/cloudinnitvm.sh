e134ea4e44b4fe7db2bba4c97f45324577b593e3
juronja <101410098+juronja@users.noreply.github.com>
2024-01-22 17:21:43 +0100

    added variables

--- Proxmox/scripts/cloudinnitvm.sh
+++ Proxmox/scripts/cloudinnitvm.sh
@@ -4,8 +4,13 @@
 # Author: juronja
 # License: MIT
 
-wget -nc --directory-prefix=/var/lib/vz/template/iso/ https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
-qm create 501 --cores 2 --cpu x86-64-v2-AES --memory 4096 --balloon 1 --name ubuntu-cloud-template --scsihw virtio-scsi-pci --net0 virtio,bridge=vmbr0 --serial0 socket --vga serial0 --ipconfig0 ip=dhcp,ip6=dhcp --agent enabled=1 --onboot 1
-qm disk import 501 /var/lib/vz/template/iso/jammy-server-cloudimg-amd64.img local-lvm --format qcow2
-qm set 501 --scsi0 local-lvm:vm-501-disk-0,discard=on,ssd=1 --ide2 local-lvm:cloudinit
-qm disk resize 501 scsi0 32G && qm set 501 --boot order=scsi0
+id=501
+cores=2
+ram=4096
+ubuntu_release="jammy"
+
+wget -nc --directory-prefix=/var/lib/vz/template/iso/ https://cloud-images.ubuntu.com/$ubuntu_release/current/$ubuntu_release-server-cloudimg-amd64.img
+qm create $id --cores 2 --cpu x86-64-v2-AES --memory 4096 --balloon 1 --name ubuntu-cloud-template --scsihw virtio-scsi-pci --net0 virtio,bridge=vmbr0 --serial0 socket --vga serial0 --ipconfig0 ip=dhcp,ip6=dhcp --agent enabled=1 --onboot 1
+qm disk import $id /var/lib/vz/template/iso/jammy-server-cloudimg-amd64.img local-lvm --format qcow2
+qm set $id --scsi0 local-lvm:vm-501-disk-0,discard=on,ssd=1 --ide2 local-lvm:cloudinit
+qm disk resize $id scsi0 32G && qm set $id --boot order=scsi0
