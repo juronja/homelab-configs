4b8f6855d2935cfc7d4c9b4520c3c6c18ec2d2f8
juronja <101410098+juronja@users.noreply.github.com>
2024-01-24 10:09:51 +0100

    added comments

--- Proxmox/scripts/cloudinnitvm.sh
+++ Proxmox/scripts/cloudinnitvm.sh
@@ -9,8 +9,17 @@ cores=2
 ram=4096
 ubuntu_release="jammy"
 
+# Dowload the Ubuntu cloud innit image
 wget -nc --directory-prefix=/var/lib/vz/template/iso/ https://cloud-images.ubuntu.com/$ubuntu_release/current/$ubuntu_release-server-cloudimg-amd64.img
+
+# Create a VM
 qm create $id --cores $cores --cpu x86-64-v2-AES --memory $ram --balloon 1 --name ubuntu-cloud-template --scsihw virtio-scsi-pci --net0 virtio,bridge=vmbr0,firewall=1 --serial0 socket --vga serial0 --ipconfig0 ip=dhcp,ip6=dhcp --agent enabled=1 --onboot 1
+
+# Import cloud image disk
 qm disk import $id /var/lib/vz/template/iso/jammy-server-cloudimg-amd64.img local-lvm --format qcow2
+
+# Map cloud image disk
 qm set $id --scsi0 local-lvm:vm-501-disk-0,discard=on,ssd=1 --ide2 local-lvm:cloudinit
+
+# Resize the disk to 32 GB.
 qm disk resize $id scsi0 32G && qm set $id --boot order=scsi0
