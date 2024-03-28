445da8e349d8d39a84fa06284095c657ac4b2307
juronja <101410098+juronja@users.noreply.github.com>
2024-03-28 20:33:29 +0100

    added disk size

--- Applications/Proxmox/scripts/cloudinnitvm.sh
+++ Applications/Proxmox/scripts/cloudinnitvm.sh
@@ -36,7 +36,16 @@ function exit-script() {
 
 echo "Starting VM script .."
 
-if CORE_COUNT=$(whiptail --backtitle "Proxmox VM install Script" --title "CORE COUNT" --radiolist "Allocate number of CPU Cores\n(To make a selection, use the Spacebar.)\n" --cancel-button "Exit Script" 10 58 2 \
+# Whiptail inputs
+if UBUNTU_RLS=$(whiptail --backtitle "Proxmox VM install Script" --title "UBUNTU RELEASE" --radiolist "Choose the Ubuntu release to install\n(Use Spacebar to select)\n" --cancel-button "Exit Script" 12 58 2 \
+    "jammy" "22.04 LTS" ON \
+    3>&1 1>&2 2>&3); then
+        echo -e "Ubuntu release version: $UBUNTU_RLS"
+else
+    exit-script
+fi
+
+if CORE_COUNT=$(whiptail --backtitle "Proxmox VM install Script" --title "CORE COUNT" --radiolist "Allocate number of CPU Cores\n(Use Spacebar to select)\n" --cancel-button "Exit Script" 12 58 2 \
     "2" "cores" ON \
     "4" "cores" OFF \
     3>&1 1>&2 2>&3); then
@@ -45,26 +54,42 @@ else
     exit-script
 fi
 
-read -p "How much RAM? Write whole numbers (e.g. 1,4,8,..): " raminput
+if RAM_COUNT=$(whiptail --backtitle "Proxmox VM install Script" --title "RAM COUNT" --radiolist "Allocate number of RAM\n(Use Spacebar to select)\n" --cancel-button "Exit Script" 12 58 3 \
+    "1" "GB" ON \
+    "2" "GB" OFF \
+    "4" "GB" OFF \
+    3>&1 1>&2 2>&3); then
+        echo -e "Allocated RAM: $RAM_COUNT GB"
+else
+    exit-script
+fi
+
+if DISK_SIZE=$(whiptail --backtitle "Proxmox VM install Script" --inputbox "Set disk size in GB" 8 58 "50" --title "DISK SIZE" --cancel-button "Exit Script" 3>&1 1>&2 2>&3); then
+    if [ -z $DISK_SIZE ]; then
+        DISK_SIZE="50"
+        echo -e "Disk size: $DISK_SIZE GB"
+    else
+        echo -e "Disk size: $DISK_SIZE GB"
+    fi
+else
+    exit-script
+fi
 
 # Constant variables
-id=501
-ram=$(($raminput * 1024))
-disk="50G"
-ubuntuRelease="jammy"
-
+VMID=501
+RAM=$(($RAM_COUNT * 1024))
 
 # Dowload the Ubuntu cloud innit image
-wget -nc --directory-prefix=/var/lib/vz/template/iso/ https://cloud-images.ubuntu.com/$ubuntuRelease/current/$ubuntuRelease-server-cloudimg-amd64.img
+wget -nc --directory-prefix=/var/lib/vz/template/iso/ https://cloud-images.ubuntu.com/$UBUNTU_RLS/current/$UBUNTU_RLS-server-cloudimg-amd64.img
 
 # Create a VM
-qm create $id --cores $CORE_COUNT --cpu x86-64-v2-AES --memory $ram --balloon 1 --name ubuntu-cloud-template --scsihw virtio-scsi-pci --net0 virtio,bridge=vmbr0,firewall=1 --serial0 socket --vga serial0 --ipconfig0 ip=dhcp,ip6=dhcp --agent enabled=1 --onboot 1
+qm create $VMID --cores $CORE_COUNT --cpu x86-64-v2-AES --memory $RAM --balloon 1 --name ubuntu-cloud-template --scsihw virtio-scsi-pci --net0 virtio,bridge=vmbr0,firewall=1 --serial0 socket --vga serial0 --ipconfig0 ip=dhcp,ip6=dhcp --agent enabled=1 --onboot 1
 
 # Import cloud image disk
-qm disk import $id /var/lib/vz/template/iso/jammy-server-cloudimg-amd64.img local-lvm --format qcow2
+qm disk import $VMID /var/lib/vz/template/iso/jammy-server-cloudimg-amd64.img local-lvm --format qcow2
 
 # Map cloud image disk
-qm set $id --scsi0 local-lvm:vm-$id-disk-0,discard=on,ssd=1 --ide2 local-lvm:cloudinit
+qm set $VMID --scsi0 local-lvm:vm-$VMID-disk-0,discard=on,ssd=1 --ide2 local-lvm:cloudinit
 
 # Resize the disk to 32 GB.
-qm disk resize $id scsi0 $disk && qm set $id --boot order=scsi0
+qm disk resize $VMID scsi0 "${DISK_SIZE}G" && qm set $VMID --boot order=scsi0
