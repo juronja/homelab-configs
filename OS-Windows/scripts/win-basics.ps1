32641ac31a63ca0fb7f95f100a0b066f3f74cdb9
juronja <101410098+juronja@users.noreply.github.com>
2024-08-30 10:42:12 +0200

    initial commit

---
+++ OS-Windows/scripts/win-basics.ps1
@@ -0,0 +1,56 @@
+# Disable UAC
+Set-itemproperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value "0" -Type DWord
+# Disable Password expiry
+wmic UserAccount set PasswordExpires=False
+# TASKBAR - Removes Widgets and Task View from the Taskbar / Alligns the taskbar to the left
+Set-itemproperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarDa" -Value "0" -Type DWord
+Set-itemproperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Value "0" -Type DWord
+Set-itemproperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Value "0" -Type DWord
+# START - Hide recently added apps / Show most used apps
+Set-itemproperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Start" -Name "ShowRecentList" -Value "0" -Type DWord
+Set-itemproperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Start" -Name "ShowFrequentList" -Value "1" -Type DWord
+# Personalize theme
+Set-itemproperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value "0" -Type DWord
+Set-itemproperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value "0" -Type DWord
+# FILE EXPLORER - Show file extensions and Show hidden folders
+Set-itemproperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Value "0" -Type DWord
+Set-itemproperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Hidden" -Value "1" -Type DWord
+# Disable Defender
+#Set-itemproperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Value "1" -Type DWord -Force
+Add-MpPreference -ExclusionPath "C:\Music_production","C:\Users\Jure\Downloads","C:\Windows","D:\","E:\","F:\","H:\","I:\","M:\","X:\"
+
+# Power settings
+powercfg /setactive SCHEME_MIN # (Min power saving)
+powercfg /hibernate on
+powercfg /change monitor-timeout-ac 20 # Wall power
+powercfg /change monitor-timeout-dc 5 # Battery
+powercfg /change standby-timeout-ac 0
+powercfg /change standby-timeout-dc 10
+powercfg /change hibernate-timeout-ac 0
+powercfg /change hibernate-timeout-dc 0
+powercfg /change disk-timeout-ac 0
+powercfg /change disk-timeout-dc 0
+
+# Install / Uninstall apps
+Get-AppxPackage -alluser Microsoft.BingNews | Remove-AppxPackage
+Get-AppxPackage -alluser Microsoft.WindowsMaps | Remove-AppxPackage
+Get-AppxPackage -alluser Microsoft.MicrosoftStickyNotes | Remove-AppxPackage
+Get-AppxPackage -alluser Microsoft.Todos | Remove-AppxPackage
+Get-AppxPackage -alluser Microsoft.YourPhone | Remove-AppxPackage
+Get-AppxPackage -alluser Microsoft.People | Remove-AppxPackage
+Get-AppxPackage -alluser Microsoft.OutlookForWindows | Remove-AppxPackage
+Get-AppxPackage -alluser Microsoft.ZuneMusic | Remove-AppxPackage
+Get-AppxPackage -alluser Microsoft.ZuneVideo | Remove-AppxPackage
+Get-AppxPackage -alluser Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
+Get-AppxPackage -alluser Microsoft.WindowsFeedbackHub | Remove-AppxPackage
+Get-AppxPackage -alluser Microsoft.WindowsSoundRecorder | Remove-AppxPackage
+Get-AppxPackage -alluser Microsoft.GetHelp | Remove-AppxPackage
+Get-AppxPackage -alluser Microsoft.Getstarted | Remove-AppxPackage
+Get-AppxPackage -alluser Clipchamp.Clipchamp | Remove-AppxPackage
+Get-AppxPackage -alluser MicrosoftCorporationII.QuickAssist | Remove-AppxPackage
+Get-AppxPackage -alluser microsoft.windowscommunicationsapps | Remove-AppxPackage # Mail app
+Get-AppxPackage -alluser Microsoft.549981C3F5F10 | Remove-AppxPackage # Cortana appGet-AppxPackage -alluser Microsoft.549981C3F5F10 | Remove-AppxPackage # Cortana app
+winget uninstall onedrive
+winget install -e --id Google.Chrome
+winget install -e --id Adobe.Acrobat.Reader.64-bit
+winget install -e --id Microsoft.PowerToys --source winget
