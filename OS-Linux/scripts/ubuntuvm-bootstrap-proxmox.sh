447afee2598b7194935508795868b3cc4bde5f87
juronja <101410098+juronja@users.noreply.github.com>
2025-03-17 16:33:18 +0100

    removed docker from minikube install

--- OS-Linux/scripts/ubuntuvm-bootstrap-proxmox.sh
+++ OS-Linux/scripts/ubuntuvm-bootstrap-proxmox.sh
@@ -158,30 +158,6 @@ if [[ $k8s == 1 ]]; then
     # Install minikube
     curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
     sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
-
-    # Remove legacy Docker
-    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-
-    # Add Docker's official GPG key:
-    sudo apt-get update
-    sudo apt-get install ca-certificates curl
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-    # Add the repository to Apt sources:
-    echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-
-    # Install Docker
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-    # Append user to docker group
-    sudo usermod -aG docker $rootUser
-    # Verify - run hello image and delete
-    sudo docker run --rm hello-world && sudo docker rmi hello-world
   fi
   # If K8S Master
   if [[ $k8sInstallType == 2 ]]; then
