79e3c1dca634adad87e30c59c3a54c752762254b
juronja <101410098+juronja@users.noreply.github.com>
2025-05-07 16:08:15 +0200

    added testing echos

--- Infrastructure/Proxmox/scripts/cloudinnitvm.sh
+++ Infrastructure/Proxmox/scripts/cloudinnitvm.sh
@@ -125,7 +125,7 @@ while true; do
 done
 
 while true; do
-  if OS_IPv4_CIDR=$(whiptail --backtitle "Install - Ubuntu VM" --inputbox "\nSet a Static IPv4 CIDR Address (/24)" 8 58 "dhcp" --title "CI IPv4 CIDR" --cancel-button "Exit Script" 3>&1 1>&2 2>&3); then
+  if OS_IPv4_CIDR=$(whiptail --backtitle "Install - Ubuntu VM" --inputbox "\nSet a Static IPv4 CIDR Address (/24)" 8 58 "dhcp" --title "CLOUD-INIT IPv4 CIDR" --cancel-button "Exit Script" 3>&1 1>&2 2>&3); then
     if [ -z $OS_IPv4_CIDR ]; then
       OS_IPv4_CIDR="dhcp"
       break
@@ -143,9 +143,11 @@ while true; do
 done
 
 if [[ $OS_IPv4_CIDR != "dhcp" ]]; then
+  echo "DEBUG: OS_IPv4_CIDR before sed: '$OS_IPv4_CIDR'"
   local SUGGESTED_GW=$(echo "$OS_IPv4_CIDR" | sed 's/\.[0-9]\{1,3\}\/\([0-9]\+\)$/.1/')
+  echo "DEBUG: SUGGESTED_GW after sed: '$SUGGESTED_GW'"
   while true; do
-    if OS_IPv4_GW=$(whiptail --backtitle "Install - Ubuntu VM" --inputbox "\nEnter gateway IP address" 8 58 "$SUGGESTED_GW" --title "CI IPv4 GATEWAY" --cancel-button "Exit Script" 3>&1 1>&2 2>&3); then
+    if OS_IPv4_GW=$(whiptail --backtitle "Install - Ubuntu VM" --inputbox "\nEnter gateway IP address" 8 58 "$SUGGESTED_GW" --title "CLOUD-INIT IPv4 GATEWAY" --cancel-button "Exit Script" 3>&1 1>&2 2>&3); then
       if [ -z $OS_IPv4_GW ]; then
           whiptail --backtitle "Install - Ubuntu VM" --msgbox "Gateway IP address cannot be empty" 8 58
       elif [[ ! "$OS_IPv4_GW" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
