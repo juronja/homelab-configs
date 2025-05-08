25353ad3393f2e95f1c8a1de8a05be169f299bb9
juronja <101410098+juronja@users.noreply.github.com>
2025-05-08 17:14:50 +0200

    cloud init parse error fix

--- Infrastructure/Proxmox/scripts/cloudinnitvm.sh
+++ Infrastructure/Proxmox/scripts/cloudinnitvm.sh
@@ -188,7 +188,8 @@ whiptail --backtitle "Install - Ubuntu VM" --title "SSH NOTE" --msgbox "Manually
 RAM=$(($RAM_COUNT * 1024))
 IMG_LOCATION="/var/lib/vz/template/iso/"
 CPU="x86-64-v3"
-CLOUD_INNIT_LOCAL="/var/lib/vz/snippets/ubuntu-homelab-cloud-init.yml"
+CLOUD_INNIT_ABSOLUTE="/var/lib/vz/snippets/ubuntu-homelab-cloud-init.yml"
+CLOUD_INNIT_LOCAL="snippets/ubuntu-homelab-cloud-init.yml"
 CLOUD_INNIT_GIT="https://raw.githubusercontent.com/juronja/homelab-configs/refs/heads/main/Infrastructure/Proxmox/ubuntu-homelab-cloud-init.yml"
 
 # Proxmox variables
@@ -215,11 +216,11 @@ qm disk resize $NEXTID scsi0 "${DISK_SIZE}G" && qm set $NEXTID --boot order=scsi
 
 # Configure Cloudinit datails
 qm set $NEXTID --ciuser $OS_USER --cipassword $OS_PASS
-qm cloudinit dump $NEXTID user > $CLOUD_INNIT_LOCAL
-# wget $CLOUD_INNIT_GIT -O temp_cloud_init.yml
-# cat temp_cloud_init.yml >> $CLOUD_INNIT_LOCAL
+qm cloudinit dump $NEXTID user > $CLOUD_INNIT_ABSOLUTE
+wget $CLOUD_INNIT_GIT -O temp_cloud_init.yml
+cat temp_cloud_init.yml >> $CLOUD_INNIT_ABSOLUTE
 qm set $NEXTID --cicustom "user=local:$CLOUD_INNIT_LOCAL"
-# rm temp_cloud_init.yml
+rm temp_cloud_init.yml
 
 # Configure Cluster level firewall rules if not enabled
 if [[ $CLUSTER_FW_ENABLED != 1 ]]; then
