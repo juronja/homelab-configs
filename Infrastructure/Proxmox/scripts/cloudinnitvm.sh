902610bcee26cd84d841b24b36ebdb4cb65a560d
juronja <101410098+juronja@users.noreply.github.com>
2025-05-09 12:21:23 +0200

    Add the repository to Apt sources

--- Infrastructure/Proxmox/scripts/cloudinnitvm.sh
+++ Infrastructure/Proxmox/scripts/cloudinnitvm.sh
@@ -253,6 +253,9 @@ if [ "$docker" == "1" ]; then
   cat <<EOF >> $CLOUD_INNIT_ABSOLUTE
   # Add Docker's official GPG key
   - sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc && sudo chmod a+r /etc/apt/keyrings/docker.asc
+  # Add the repository to Apt sources
+  - echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  - sudo apt-get update
 EOF
 fi
 # if [[ $registries == 1 ]]; then
@@ -262,10 +265,8 @@ fi
 # EOF
 # fi
 
+
 # #####
-#   # Add the repository to Apt sources
-#   - echo "deb [arch=\$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \$(. /etc/os-release && echo "\$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#   - sudo apt-get update
 #   # Install Docker
 #   - sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 #   # Append user to docker group
