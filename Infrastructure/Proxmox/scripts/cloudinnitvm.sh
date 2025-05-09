c39de42b2cfacc031a678f2645196647e3b512c3
juronja <101410098+juronja@users.noreply.github.com>
2025-05-09 15:14:12 +0200

    added apt get update before install

--- Infrastructure/Proxmox/scripts/cloudinnitvm.sh
+++ Infrastructure/Proxmox/scripts/cloudinnitvm.sh
@@ -255,6 +255,7 @@ if [ "$docker" == "1" ]; then
   - curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc && chmod a+r /etc/apt/keyrings/docker.asc
   # Add the repository to Apt sources
   - echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  - apt-get update
   # Install Docker
   - apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 EOF
