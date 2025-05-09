603c6ef2c8f4232f7bd9c15245c1932f5f6fb5e0
juronja <101410098+juronja@users.noreply.github.com>
2025-05-09 16:30:49 +0200

    fix for os user variable

--- Infrastructure/Proxmox/scripts/cloudinnitvm.sh
+++ Infrastructure/Proxmox/scripts/cloudinnitvm.sh
@@ -250,7 +250,7 @@ cat <<EOF >> $CLOUD_INNIT_ABSOLUTE
 EOF
 # Docker install
 if [ "$docker" == "1" ]; then
-  cat <<'EOF' >> "$CLOUD_INNIT_ABSOLUTE"
+  cat <<'EOF' >> $CLOUD_INNIT_ABSOLUTE
   # Add Docker's official GPG key
   - curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc && chmod a+r /etc/apt/keyrings/docker.asc
   # Add the repository to Apt sources
@@ -258,6 +258,8 @@ if [ "$docker" == "1" ]; then
   - apt-get update
   # Install Docker
   - apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+EOF
+  cat <<EOF >> $CLOUD_INNIT_ABSOLUTE
   # Append user to docker group
   - sudo usermod -aG docker "$OS_USER"
 EOF
