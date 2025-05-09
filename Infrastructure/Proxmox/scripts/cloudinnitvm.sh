2726b334eb0328ae17a77a71ccf4f14c704ba015
juronja <101410098+juronja@users.noreply.github.com>
2025-05-09 11:51:37 +0200

    GPG key added

--- Infrastructure/Proxmox/scripts/cloudinnitvm.sh
+++ Infrastructure/Proxmox/scripts/cloudinnitvm.sh
@@ -252,6 +252,7 @@ EOF
 if [ "$docker" == "1" ]; then
   cat <<EOF >> $CLOUD_INNIT_ABSOLUTE
   # Add Docker's official GPG key
+  - sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc && sudo chmod a+r /etc/apt/keyrings/docker.asc
 EOF
 fi
 # if [[ $registries == 1 ]]; then
@@ -262,11 +263,6 @@ fi
 # fi
 
 # #####
-#   # Add Docker's official GPG key
-#   - sudo apt-get install ca-certificates curl
-#   - sudo install -m 0755 -d /etc/apt/keyrings
-#   - sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-#   - sudo chmod a+r /etc/apt/keyrings/docker.asc
 #   # Add the repository to Apt sources
 #   - echo "deb [arch=\$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \$(. /etc/os-release && echo "\$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 #   - sudo apt-get update
