9733a833883e78be44f1d19f9f8520ddd00f34b0
juronja <101410098+juronja@users.noreply.github.com>
2025-06-06 16:47:52 +0200

    removed variable

--- OpenWRT/scripts/cloudflare-ips-set.sh
+++ OpenWRT/scripts/cloudflare-ips-set.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-LOG_TAG="cloudflare_ip_update_script"
-
-wget -q "https://www.cloudflare.com/ips-v4" -O ->> /tmp/cloudflare-ips.txt
+wget -q "https://www.cloudflare.com/ips-v4" -O - > /tmp/cloudflare-ips.txt
 
 if [ $? -eq 0 ]; then
-    logger -t "${LOG_TAG}" "INFO: Cloudflare IPs downloaded successfully to /tmp/cloudflare-ips.txt"
+    logger -t "cloudflare_ip_update_script" "INFO: Cloudflare IPs downloaded successfully to /tmp/cloudflare-ips.txt"
 else
-    logger -t "${LOG_TAG}" "ERROR: Failed to download Cloudflare IPs. Wget exit code: $?."
+    logger -t "cloudflare_ip_update_script" "ERROR: Failed to download Cloudflare IPs. Wget exit code: $?."
     # Exit if wget fails to prevent further commands from running with outdated data
     exit 1
 fi
