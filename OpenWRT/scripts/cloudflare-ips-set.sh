3b274e450c170195528db553c1ce4d9f78931c5a
juronja <101410098+juronja@users.noreply.github.com>
2025-06-06 17:12:32 +0200

    added one more variable

--- OpenWRT/scripts/cloudflare-ips-set.sh
+++ OpenWRT/scripts/cloudflare-ips-set.sh
@@ -8,7 +8,7 @@ rm /tmp/$FILE_NAME
 # wget -q "https://www.cloudflare.com/ips-v4" -O - > /tmp/$FILE_NAME
 
 if [ $? -eq 0 ]; then
-    logger -p notice -t $LOG_TAG "INFO: Cloudflare IPs downloaded successfully to /tmp/cloudflare-ips.txt"
+    logger -p notice -t $LOG_TAG "INFO: Cloudflare IPs downloaded successfully to /tmp/$FILE_NAME"
 else
     logger -p err -t $LOG_TAG "ERROR: Failed to download Cloudflare IPs. Wget exit code: $?."
     # Exit if wget fails to prevent further commands from running with outdated data
