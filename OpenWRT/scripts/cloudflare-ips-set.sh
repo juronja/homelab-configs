fe451d3ef760d524540ac6c87ac33a374004a32f
juronja <101410098+juronja@users.noreply.github.com>
2025-06-06 16:54:12 +0200

    added priority

--- OpenWRT/scripts/cloudflare-ips-set.sh
+++ OpenWRT/scripts/cloudflare-ips-set.sh
@@ -3,9 +3,9 @@
 wget -q "https://www.cloudflare.com/ips-v4" -O - > /tmp/cloudflare-ips.txt
 
 if [ $? -eq 0 ]; then
-    logger -t "cloudflare_ip_update_script" "INFO: Cloudflare IPs downloaded successfully to /tmp/cloudflare-ips.txt"
+    logger -p notice -t cloudflare_ip_update_script "INFO: Cloudflare IPs downloaded successfully to /tmp/cloudflare-ips.txt"
 else
-    logger -t "cloudflare_ip_update_script" "ERROR: Failed to download Cloudflare IPs. Wget exit code: $?."
+    logger -p err -t cloudflare_ip_update_script "ERROR: Failed to download Cloudflare IPs. Wget exit code: $?."
     # Exit if wget fails to prevent further commands from running with outdated data
     exit 1
 fi
