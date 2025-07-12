183680e1bcbc80edfb64c871299e36110d1d1dfc
juronja <101410098+juronja@users.noreply.github.com>
2025-07-12 14:04:14 +0200

    commented non functioning code

--- Infrastructure/Terraform/eks/main.tf
+++ Infrastructure/Terraform/eks/main.tf
@@ -54,7 +54,7 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.36.0"
+  version = "20.37.1"
 
   cluster_name    = "${var.project}-cluster"
   cluster_version = "1.33"
@@ -71,6 +71,14 @@ module "eks" {
   cluster_endpoint_public_access = true
   cluster_endpoint_public_access_cidrs = [ "0.0.0.0/0" ] # Limit to your IP if needed
 
+  # # AWS Load Balancer Controller
+  # enable_irsa = true
+  # cluster_addons = {
+  #   aws-load-balancer-controller = {
+  #     most_recent = true # Use the latest version of the add-on
+  #   }
+  # }  
+
   # Node group
   eks_managed_node_groups = {
     nodegroup1 = {
@@ -85,10 +93,52 @@ module "eks" {
 
   # Override defaults
   create_cloudwatch_log_group = false
-  enable_irsa = false
   # create_kms_key = false
 
   tags = {
     Environment = var.environment
   }
 }
+
+#####################################
+## ALB Load Balancer configuration ##
+#####################################
+
+# NOTE: The AWS Load Balancer Controller will automatically create and manage
+# Target Groups and Listener Rules for you based on Kubernetes Ingress objects.
+#
+# You are defining the main ALB resource here. The controller will then add rules to it.
+
+# module "alb" {
+#   source  = "terraform-aws-modules/alb/aws"
+#   version = "9.17.0"
+
+#   name = "${var.project}-alb-${var.environment}"
+#   load_balancer_type = "application"
+#   vpc_id = module.vpc.vpc_id
+#   subnets = module.vpc.public_subnets
+
+#   # Listeners for the ALB
+#   listeners = {
+#     # This listener will forward traffic to target groups created by the controller
+#     http = {
+#       port     = 80
+#       protocol = "HTTP"
+#       # A default action is required. The controller will add rules to override this.
+#       default_action = {
+#         type = "fixed-response"
+#         fixed_response = {
+#           content_type = "text/plain"
+#           message_body = "Cannot route request"
+#           status_code  = "404"
+#         }
+#       }
+#     }
+#   }
+
+#   tags = {
+#     Environment = var.environment
+#   }
+# }
+
+
