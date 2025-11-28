d5d9e82c0c7be0fcc58f35e7073140bd05c23818
juronja <101410098+juronja@users.noreply.github.com>
2025-11-28 09:04:08 +0000

    added USB suspend

--- OS-Windows/scripts/win-install-basics.ps1
+++ OS-Windows/scripts/win-install-basics.ps1
@@ -89,6 +89,7 @@ Confirm-Step -Description "Configuring Power settings..." -Action {
     powercfg /change hibernate-timeout-dc 0
     powercfg /change disk-timeout-ac 0
     powercfg /change disk-timeout-dc 0
+    powercfg /SETACVALUEINDEX SCHEME_CURRENT 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0 # USB Suspend AC
         
     Write-Host "✔️ Power settings applied." -ForegroundColor Green
 }
